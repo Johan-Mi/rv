c753cdac2d878f0b452b40b1bdd8ad063e9c69ce
Johan-Mi <johanmi@protonmail.com>
2022-10-13 18:04:06 +0200

    Fix ELF detection

--- src/load.rs
+++ src/load.rs
@@ -10,15 +10,19 @@ pub fn load_program(
     path: &Path,
 ) -> Result<(Vec<u8>, *const u16), Box<dyn std::error::Error>> {
     let mut file = File::open(path)?;
-    match ::elf::File::open_stream(&mut file) {
-        Err(::elf::ParseError::InvalidMagic) => {
-            // Not an ELF; treat it as a flat binary
-            file.rewind()?;
-            let mut program_bytes = Vec::new();
-            file.read_to_end(&mut program_bytes)?;
-            let pc = program_bytes.as_ptr().cast();
-            Ok((program_bytes, pc))
-        }
-        res => Ok(elf::load_elf_file(res.unwrap())),
+    let mut elf_magic_number = [0; 4];
+    if file.read(&mut elf_magic_number)? == 4 && &elf_magic_number == b"\x7fELF"
+    {
+        file.rewind()?;
+        Ok(elf::load_elf_file(
+            ::elf::File::open_stream(&mut file).unwrap(),
+        ))
+    } else {
+        // Not an ELF; treat it as a flat binary
+        file.rewind()?;
+        let mut program_bytes = Vec::new();
+        file.read_to_end(&mut program_bytes)?;
+        let pc = program_bytes.as_ptr().cast();
+        Ok((program_bytes, pc))
     }
 }
