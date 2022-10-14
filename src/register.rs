4f4494ff1bac24c552bfa841ccb0322976352c77
Johan-Mi <johanmi@protonmail.com>
2022-10-14 19:09:48 +0200

    Make `RegisterName` debug impl prettier

--- src/register.rs
+++ src/register.rs
@@ -1,6 +1,8 @@
+use std::fmt;
+
 use crate::bits::{u16_sms, u32_sms};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct RegisterName(u8);
 
 impl RegisterName {
@@ -25,6 +27,19 @@ impl RegisterName {
 
     pub const fn compressed_rs2(word: u16) -> Self {
         Self(u16_sms(word, 2, 5, 0) as u8)
+    }
+}
+
+impl fmt::Debug for RegisterName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(
+            [
+                "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "fp", "s1",
+                "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3",
+                "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4",
+                "t5", "t6",
+            ][usize::from(self.0)],
+        )
     }
 }
 
