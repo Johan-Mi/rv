7f3d4a9ffae4894db5c33a9096a329a9cae51dfb
Johan-Mi <johanmi@protonmail.com>
2022-10-15 12:20:59 +0200

    Factor out ELF section filtering

--- src/load/elf.rs
+++ src/load/elf.rs
@@ -1,28 +1,25 @@
 pub fn load_elf_file(file: elf::File) -> (Vec<u8>, *const u16) {
+    // Sections with an address of zero should not be loaded
+    let relevant_sections = || {
+        file.sections
+            .iter()
+            .filter(|section| section.shdr.addr != 0)
+    };
+
     // Save some memory by moving all of the section starting addresses so that
     // the lowest one ends up at zero
-    let address_offset = file
-        .sections
-        .iter()
+    let address_offset = relevant_sections()
         .map(|section| section.shdr.addr)
-        .filter(|&addr| addr != 0)
         .min()
         .unwrap_or_default();
 
-    let total_size = file
-        .sections
-        .iter()
-        .filter(|section| section.shdr.addr != 0)
+    let total_size = relevant_sections()
         .map(|section| section.shdr.addr - address_offset + section.shdr.size)
         .max()
         .unwrap_or_default() as usize;
     let mut bytes = vec![0u8; total_size];
 
-    for section in file
-        .sections
-        .iter()
-        .filter(|section| section.shdr.addr != 0)
-    {
+    for section in relevant_sections() {
         bytes[(section.shdr.addr - address_offset) as usize..]
             [..section.shdr.size as usize]
             .copy_from_slice(&section.data);
