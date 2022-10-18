e6dc23224e147d7429df9a8cf814a8447716314f
Johan-Mi <johanmi@protonmail.com>
2022-10-18 08:25:19 +0200

    Load ELFs by segments, not sections

--- src/load/elf.rs
+++ src/load/elf.rs
@@ -1,32 +1,38 @@
+use elf::types::PT_LOAD;
 use std::io::Cursor;
 
 pub fn load_elf_file(raw_file: &[u8]) -> (Vec<u8>, *const u16) {
     let file = elf::File::open_stream(&mut Cursor::new(raw_file)).unwrap();
 
-    // Sections with an address of zero should not be loaded
-    let relevant_sections = || {
-        file.sections
-            .iter()
-            .filter(|section| section.shdr.addr != 0)
-    };
-
-    // Save some memory by moving all of the section starting addresses so that
+    // Save some memory by moving all of the segment starting addresses so that
     // the lowest one ends up at zero
-    let address_offset = relevant_sections()
-        .map(|section| section.shdr.addr)
+    let address_offset = file
+        .phdrs
+        .iter()
+        .map(|segment| segment.vaddr)
         .min()
         .unwrap_or_default();
 
-    let total_size = relevant_sections()
-        .map(|section| section.shdr.addr - address_offset + section.shdr.size)
+    let total_size = file
+        .phdrs
+        .iter()
+        .map(|segment| segment.vaddr - address_offset + segment.memsz)
         .max()
         .unwrap_or_default() as usize;
     let mut bytes = vec![0u8; total_size];
 
-    for section in relevant_sections() {
-        bytes[(section.shdr.addr - address_offset) as usize..]
-            [..section.shdr.size as usize]
-            .copy_from_slice(&section.data);
+    for segment in file.phdrs.iter() {
+        match segment.progtype {
+            PT_LOAD => {
+                bytes[(segment.vaddr - address_offset) as usize..]
+                    [..segment.filesz as usize]
+                    .copy_from_slice(
+                        &raw_file[segment.offset as usize..]
+                            [..segment.filesz as usize],
+                    );
+            }
+            _ => {}
+        }
     }
 
     // Convert the entry address requested in the ELF to where it actually gets
